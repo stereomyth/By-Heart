c3f3a6009a19c5ef8f06c0729d8831fda484bfd6
sorakasumi <j@lostwith.me>
2012-07-18 16:57:44 +0100

    added .gitignore
    now i understand gitignore!

--- popup.js
+++ popup.js
@@ -120,7 +120,18 @@ function buildSearchList(searchList) {
 document.addEventListener('DOMContentLoaded', function () {
     getBookmarkTree();
     localStuff();
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-33494524-1']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = 'https://ssl.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
 });
+
+
 
 //todo: local storage
 //todo: histroy?
