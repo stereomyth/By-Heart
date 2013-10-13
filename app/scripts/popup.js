6957734505f462a6d08076160e9ced7859a306d2
James King <j@lostwith.me>
2013-10-13 20:38:36 +0100

    added search function and basic results listing

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,27 +1,93 @@
-'use strict';
-
 var popup = {
 
     ls: localStorage,
 
-    popup: popup,
+    // popup: popup,   // ???
 
     init: function () {
 
-        popup.testjs();
+        popup.listnerInit();
 
         return this;
+
     },
 
-    testjs: function () {
+    listnerInit: function () {
 
-        console.log("...i remember");
+        $('.search').on('keyup', function() {
+
+            popup.search($(this).val());
+
+        });
+
+    },
+
+    search: function (query) {
+
+        chrome.bookmarks.search(query, function (bookmarksArray) {
+
+            popup.buildList(bookmarksArray);
+
+        });
+
+    },
+
+    buildList: function (bookmarksArray) {
+
+        $('.bookmarks').empty();
+
+        var howMany = (bookmarksArray.length > 10) ? 10 : bookmarksArray.length;
+
+        for (var i = 0; i < howMany; i++) {
+
+            // console.log(bookmarksArray[i].title);
+
+            var thisBookmark = $.extend(bookmark, {
+
+                datas: bookmarksArray[i],
+
+            });
+
+            thisBookmark.init();
+
+            $('.bookmarks').append(thisBookmark.domObject);
+
+        }
 
     }
 
 };
 
+var bookmark = {
+
+    datas: {},
+
+    init: function () {
+
+        bookmark.unfurlDatas();
+
+        bookmark.domObject = $('<a/>', {
+
+                'href': bookmark.url,
+
+                'text': bookmark.title,
+
+            });
+
+
+    },
+
+    unfurlDatas: function (argument) {
+
+        bookmark.title = bookmark.datas.title;
+        bookmark.url = bookmark.datas.url;
+
+    }
+
+}
+
 $(document).ready(function () {
+    'use strict';
 
     popup.init();
 
